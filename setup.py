6d11fc9f1e5e56d4feab8a8872cea04d772aa9dd
glyph <>
2005-10-10 22:56:52 +0000

    distutils magic and package version information

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from distutils.core import setup
+
+setup(
+    name="Vertex",
+    version="0.1",
+    maintainer="Divmod, Inc.",
+    maintainer_email="support@divmod.org",
+    url="http://divmod.org/trac/wiki/VertexProject",
+    license="MIT",
+    platforms=["any"],
+    description="A Q2Q protocol implementation",
+    classifiers=(
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Internet"),
+
+    packages=['vertex',
+              'vertex.scripts',
+              'vertex.test'],
+
+    scripts=['bin/vertex', 'bin/certcreate'])
--- vertex/__init__.py
+++ vertex/__init__.py
@@ -1,4 +1,5 @@
 # -*- test-case-name: vertex.test -*-
 
-"""
-"""
+from epsilon.versions import Version
+
+version = Version(__name__, 0, 1, 0)
