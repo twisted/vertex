4dc5720367d2331bcef0eb03fa280c52d668c361
Glyph <glyph@twistedmatrix.com>
2016-12-02 23:12:20 -0800

    steal the twistedchecker script from twisted

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,14 +2,8 @@
 case "${TOXENV}" in
     lint)
         pyflakes vertex bin prime
-        rm -fr .baseline
-        mkdir .baseline
-        (
-            cd .baseline
-            git --work-tree . checkout origin/master .
-            twistedchecker vertex > ../.baseline.result
-        )
-        twistedchecker --diff=.baseline.result vertex
+        pip install diff_cover
+        bash .travis/twistedchecker-trunk-diff.sh vertex
         ;;
     py27)
         coverage run `which trial` vertex
